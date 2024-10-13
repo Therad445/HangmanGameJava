e3e10d1492a5fb524f16636ffba41e4cbb0d9cfd
Радмир Исламов <therad445@outlook.com>
2024-10-13 16:41:55 +0300

    refactor: use try-with-resources for Scanner

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -12,29 +12,32 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
 
-        Category categories = new Category(Words.getHangmanWords());
-        log.info("Выберите категорию:\n{}", String.join(", ", categories.getCategoriesList()));
-        String category = scanner.nextLine().toLowerCase();
-        log.info((categories.setUsingCategory(category)));
+            Category categories = new Category(Words.getHangmanWords());
+            log.info("Выберите категорию:\n{}", String.join(", ", categories.getCategoriesList()));
+            String category = scanner.nextLine().toLowerCase();
+            log.info((categories.setUsingCategory(category)));
 
-        log.info("Выберете сложность:\nлегко, средне, сложно");
-        Difficulty difficulties = new Difficulty();
-        difficulties.setDifficulty(scanner.nextLine().toLowerCase());
+            log.info("Выберете сложность:\nлегко, средне, сложно");
+            Difficulty difficulties = new Difficulty();
+            difficulties.setDifficulty(scanner.nextLine().toLowerCase());
 
-        Words words = new Words(Words.getHangmanWords().get(category));
-        log.info("Слово выбрано.");
+            Words words = new Words(Words.getHangmanWords().get(category));
+            log.info("Слово выбрано.");
 
-        HangmanGame game = new HangmanGame(categories.usingCategory(), words.usingWord(), difficulties.attemptsLeft());
-        log.info("Игра началась:");
-        while (!game.gameOver()) {
-            game.displayInfo();
-            log.info("Ввведите букву:");
-            char symbol = scanner.nextLine().charAt(0);
-            game.inputChar(symbol);
+            HangmanGame game = new HangmanGame(categories.usingCategory(), words.usingWord(), difficulties.attemptsLeft());
+            log.info("Игра началась:");
+            while (!game.gameOver()) {
+                game.displayInfo();
+                log.info("Ввведите букву:");
+                char symbol = scanner.nextLine().charAt(0);
+                game.inputChar(symbol);
+            }
         }
-        scanner.close();
+        catch (Exception e) {
+            log.error("Произошла ошибка: {}", e.getMessage());
+        }
     }
 }
 
